deab9f3a7626cc6ee27b620bc7e6d75e1bdaa8ea
Wenxi Jin <wenxi.jin@microsemi.com>
2016-06-03 10:12:31 +0200

    Enabled gtags, c-c++ layers. gtags has to be enabled in order to use helm-gtags.

--- init.el
+++ init.el
@@ -37,6 +37,10 @@ values."
      version-control
      cscope
      asciidoc
+     gtags
+     (c-c++ :variables
+            c-c++-default-mode-for-headers 'c++-mode
+            c-c++-enable-clang-support t)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
