c1694840d3a67cc241d5e889870bf9bb565549e2
Wenxi Jin <jwenxi@gmail.com>
2016-08-30 20:27:44 +0200

    Enabled new layers: python, java

--- init.el
+++ init.el
@@ -47,6 +47,8 @@ values."
      (c-c++ :variables
             c-c++-default-mode-for-headers 'c++-mode
             c-c++-enable-clang-support t)
+     python
+     java
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
