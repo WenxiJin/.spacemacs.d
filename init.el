e21c3bf0faa9e222dac5d6f9e45e24a4d4f48cd6
Wenxi Jin <wenxi.jin@biolinscientific.com>
2016-09-01 13:55:09 +0200

    Changed theme, changed tab-width in java.
    Changed the default spacemacs theme.
    Allow indent-tab-mode in java.
    Set tab-width to 8 in java.
    eclim bug is fixed by using spacemacs develop branch.

--- init.el
+++ init.el
@@ -98,7 +98,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
+   dotspacemacs-themes '(monokai
+                         spacemacs-dark
                          spacemacs-light
                          solarized-light
                          solarized-dark
@@ -221,6 +222,11 @@ user code."
  This function is called at the very end of Spacemacs initialization after
 layers configuration. You are free to put any user code."
   (xterm-mouse-mode -1)
+  (setq-default tab-width 8)
+  (add-hook 'java-mode-hook
+            (lambda ()
+              (setq indent-tabs-mode t)
+              (setq tab-width 8)))
   (helm-projectile-on)  ;; replace projectile cmds
   (setq projectile-switch-project-action 'helm-projectile)
   (global-set-key (kbd "C-x C-f") 'spacemacs/helm-find-files)
