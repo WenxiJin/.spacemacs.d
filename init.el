85796142cfe1c864b7d7351a5ebf5a0e38d7d5c7
Wenxi Jin <wenxi.jin@biolinscientific.com>
2016-09-02 14:16:33 +0200

    Enabled nyan-mode.

--- init.el
+++ init.el
@@ -53,6 +53,9 @@ values."
      java
      ;; applications
      ranger
+     ;; themes/colors
+     (colors :variables
+             colors-enable-nyan-cat-progress-bar t)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
