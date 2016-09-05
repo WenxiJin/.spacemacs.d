200cc65153f739b00752d192f0dc5b079555b691
Wenxi Jin <wenxi.jin@biolinscientific.com>
2016-09-05 10:03:33 +0200

    Changed default themes.
    Added some dark themes as default options.

--- init.el
+++ init.el
@@ -102,13 +102,13 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
-                         spacemacs-light
-                         solarized-light
-                         solarized-dark
-                         leuven
+   dotspacemacs-themes '(lush
                          monokai
-                         zenburn)
+                         ample
+                         spacemacs-dark
+                         afternoon
+                         molokai
+                         )
    ;; If non nil the cursor color matches the state color.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
