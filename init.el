169456dd2e90f3cb8cc96e984042f8373fcbcc2f
Wenxi Jin <wenxi.jin@biolinscientific.com>
2016-09-06 13:08:26 +0200

    Simplified theme options.
    Spacemacs-dark, monokai are my favorite now.

--- init.el
+++ init.el
@@ -99,12 +99,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(lush
+   dotspacemacs-themes '(spacemacs-dark
                          monokai
-                         ample
-                         spacemacs-dark
-                         afternoon
-                         molokai
                          )
    ;; If non nil the cursor color matches the state color.
    dotspacemacs-colorize-cursor-according-to-state t
