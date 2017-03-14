4678d3cc95dbc34c1e0b27dfad77a3481ba99265
Wenxi Jin <wenxi.jin@biolinscientific.com>
2017-03-14 19:31:08 +0100

    Installed google-c-style package

--- init.el
+++ init.el
@@ -75,7 +75,8 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(vlf)
+   dotspacemacs-additional-packages '(vlf
+                                      google-c-style)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
