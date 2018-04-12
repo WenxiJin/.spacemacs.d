1e163293f8168e1cb83612f3f81130133f43a9af
Wenxi Jin <wjn@sophion.com>
2018-04-12 11:15:16 +0200

    Removed monokai-alt theme

--- init.el
+++ init.el
@@ -151,7 +151,6 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(spacemacs-dark
-                         monokai-alt
                          atom-one-dark
                          afternoon)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
